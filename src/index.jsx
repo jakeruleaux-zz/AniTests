ae4006dbfe5c1fa73a6669938b8a92c087819188
jacob ruleaux <jakeruleaux@gmail.com>
2017-09-27 11:39:17 -0700

    intitial commit with deps installed

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import styles from "./styles/styles.css";
+
+ReactDOM.render(
+  <App />,
+  document.getElementById("react-app-root")
+);
