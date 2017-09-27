57311ad631ae91d954116d4acc4df51a6fc7c9d7
jacob ruleaux <jakeruleaux@gmail.com>
2017-09-27 16:37:33 -0700

    final version for day

--- src/components/Entered.jsx
+++ src/components/Entered.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import LogInForm from './LogInForm';
+import styles from './../styles/Entered.css';
 
 class Entered extends React.Component {
   render(){
     return (
-      <div>
-      entered
-      <LogInForm entered={true} {...this.props}/>
+      <div className = {styles.userinput}>
+        <p>you logged in</p>
+        <LogInForm entered={true} {...this.props}/>
       </div>
     )
   }
